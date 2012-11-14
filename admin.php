f4a711071f3027fc99780cb9aa6376f879e2925a
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-14 03:50:25 +0000

    * Version 5.2 	* Add custom fields in all the tables
    git-svn-id: https://plugins.svn.wordpress.org/contact-manager/tags/5.2@625030 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,104 @@
+<?php if (strstr($_SERVER['REQUEST_URI'], '/plugins.php')) { load_plugin_textdomain('contact-manager', false, 'contact-manager/languages'); }
+if ((isset($_GET['page'])) && (strstr($_GET['page'], 'contact-manager'))) { include_once dirname(__FILE__).'/admin-pages-functions.php'; }
+
+
+function contact_manager_admin_menu() {
+include dirname(__FILE__).'/admin-pages.php';
+$options = (array) get_option('contact_manager_back_office');
+if (((isset($_GET['page'])) && (strstr($_GET['page'], 'contact-manager'))) || ($options['menu_title'] == '')) { $options['menu_title'] = __('Contact', 'contact-manager'); }
+$menu_items = (array) $options['menu_items'];
+$numbers = (array) $options['menu_displayed_items'];
+$menu_displayed_items = array();
+foreach ($numbers as $i) { $menu_displayed_items[] = $menu_items[$i]; }
+if ((defined('CONTACT_MANAGER_DEMO')) && (CONTACT_MANAGER_DEMO == true)) { $capability = 'manage_options'; }
+else { $role = $options['minimum_roles']['view']; $capability = $roles[$role]['capability']; }
+if ($options['custom_icon_used'] == 'yes') { $icon_url = format_url($options['custom_icon_url']); } else { $icon_url = ''; }
+add_menu_page('Contact Manager', $options['menu_title'], $capability, 'contact-manager', create_function('', 'include_once "options-page.php";'), $icon_url);
+foreach ($admin_pages as $key => $value) {
+$slug = 'contact-manager'.($key == '' ? '' : '-'.str_replace('_', '-', $key));
+if ((!isset($_GET['page'])) || (!strstr($_GET['page'], 'contact-manager'))) { $value['menu_title'] = $options['pages_titles'][$key]; }
+if (($key == '') || ($key == 'back_office') || ((isset($_GET['page'])) && ($_GET['page'] == $slug)) || (in_array($key, $menu_displayed_items))) {
+add_submenu_page('contact-manager', $value['page_title'], $value['menu_title'], $capability, $slug, create_function('', 'include_once "'.$value['file'].'";')); } } }
+
+add_action('admin_menu', 'contact_manager_admin_menu');
+
+
+function contact_manager_action_links($links, $file) {
+if ($file == 'contact-manager/contact-manager.php') {
+if (!is_multisite()) {
+$links = array_merge($links, array(
+'<a href="admin.php?page=contact-manager&amp;action=uninstall">'.__('Uninstall', 'contact-manager').'</a>')); }
+$links = array_merge($links, array(
+'<a href="admin.php?page=contact-manager&amp;action=reset">'.__('Reset', 'contact-manager').'</a>',
+'<a href="admin.php?page=contact-manager">'.__('Options', 'contact-manager').'</a>')); }
+return $links; }
+
+add_filter('plugin_action_links', 'contact_manager_action_links', 10, 2);
+
+
+function contact_manager_row_meta($links, $file) {
+if ($file == 'contact-manager/contact-manager.php') {
+$links = array_merge($links, array(
+'<a href="http://www.kleor-editions.com/contact-manager">'.__('Documentation', 'contact-manager').'</a>')); }
+return $links; }
+
+add_filter('plugin_row_meta', 'contact_manager_row_meta', 10, 2);
+
+
+function install_contact_manager() {
+global $wpdb;
+load_plugin_textdomain('contact-manager', false, 'contact-manager/languages');
+include dirname(__FILE__).'/initial-options.php';
+foreach ($initial_options as $key => $value) {
+$_key = ($key == '' ? '' : '_'.$key);
+if (is_array($value)) {
+$options = (array) get_option('contact_manager'.$_key);
+if ((isset($options[0])) && ($options[0] === false)) { unset($options[0]); }
+foreach ($value as $option => $initial_value) {
+if (($option == 'menu_title') || ($option == 'pages_titles') || (!isset($options[$option]))
+ || ($options[$option] == '')) { $options[$option] = $initial_value; } }
+update_option('contact_manager'.$_key, $options); }
+else { add_option(substr('contact_manager'.$_key, 0, 64), $value); } }
+
+include_once ABSPATH.'wp-admin/includes/upgrade.php';
+$charset_collate = '';
+if (!empty($wpdb->charset)) { $charset_collate .= 'DEFAULT CHARACTER SET '.$wpdb->charset; }
+if (!empty($wpdb->collate)) { $charset_collate .= ' COLLATE '.$wpdb->collate; }
+include dirname(__FILE__).'/tables.php';
+foreach ($tables as $table_slug => $table) {
+$list = ''; foreach ($table as $key => $value) { $list .= "
+".$key." ".$value['type']." ".($key == "id" ? "auto_increment" : "NOT NULL").","; }
+$sql = "CREATE TABLE ".$wpdb->prefix."contact_manager_".$table_slug." (".$list."
+PRIMARY KEY  (id)) $charset_collate;"; dbDelta($sql);
+foreach ($table as $key => $value) { if (isset($value['default'])) {
+$results = $wpdb->query("UPDATE ".$wpdb->prefix."contact_manager_".$table_slug." SET ".$key." = '".$value['default']."' WHERE ".$key." = ''"); } } }
+foreach ($tables as $table_slug => $table) {
+$error = (((isset($error)) && ($error == 'yes')) ? 'yes' : 'no');
+$columns_number = (int) mysql_num_fields(mysql_query("SELECT * FROM ".$wpdb->prefix."contact_manager_".$table_slug." WHERE id = 0"));
+if ($columns_number < count($table)) { $error = 'yes'; } }
+if ($error == 'no') {
+$options = (array) get_option('contact_manager');
+$options['version'] = CONTACT_MANAGER_VERSION;
+update_option('contact_manager', $options); } }
+
+register_activation_hook('contact-manager/contact-manager.php', 'install_contact_manager');
+
+
+function reset_contact_manager() {
+load_plugin_textdomain('contact-manager', false, 'contact-manager/languages');
+include dirname(__FILE__).'/initial-options.php';
+foreach ($initial_options as $key => $value) {
+$_key = ($key == '' ? '' : '_'.$key);
+update_option(substr('contact_manager'.$_key, 0, 64), $value); }
+install_contact_manager(); }
+
+
+function uninstall_contact_manager() {
+global $wpdb;
+include dirname(__FILE__).'/initial-options.php';
+foreach ($initial_options as $key => $value) {
+$_key = ($key == '' ? '' : '_'.$key);
+delete_option(substr('contact_manager'.$_key, 0, 64)); }
+include dirname(__FILE__).'/tables.php';
+foreach ($tables as $table_slug => $table) {
+$results = $wpdb->query("DROP TABLE ".$wpdb->prefix.'contact_manager_'.$table_slug); } }
---
+++ item-data.php
@@ -0,0 +1,58 @@
+<?php global $wpdb;
+if (strstr($type, 'category')) { $attribute = 'category'; } else { $attribute = 'id'; }
+switch ($type) {
+case 'contact_form': $table = 'forms'; $default_field = 'name'; break;
+case 'contact_form_category': $table = 'forms_categories'; $default_field = 'name'; break;
+case 'message': $table = 'messages'; $default_field = 'subject'; break; }
+$_GET[$type.'_data'] = (array) (isset($_GET[$type.'_data']) ? $_GET[$type.'_data'] : array());
+if ((isset($_GET[$type.'_id'])) && ((!isset($_GET[$type.'_data']['id'])) || ($_GET[$type.'_data']['id'] != $_GET[$type.'_id']))) {
+$n = $_GET[$type.'_id']; $_GET[$type.$n.'_data'] = (array) (isset($_GET[$type.$n.'_data']) ? $_GET[$type.$n.'_data'] : array());
+if ((isset($_GET[$type.$n.'_data']['id'])) && ($_GET[$type.$n.'_data']['id'] == $_GET[$type.'_id'])) { $_GET[$type.'_data'] = $_GET[$type.$n.'_data']; }
+else { $_GET[$type.'_data'] = (array) $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."contact_manager_".$table." WHERE id = ".$_GET[$type.'_id'], OBJECT); } }
+if (!is_admin()) {
+if (($type == 'message') && (!isset($_GET[$type.'_data']['email_address']))) {
+$result = $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."contact_manager_".$table." WHERE ip_address = '".$_SERVER['REMOTE_ADDR']."' ORDER BY date DESC LIMIT 1", OBJECT);
+if ($result) { $_GET[$type.'_data'] = (array) $result; } }
+if (isset($_GET[$type.'_data']['id'])) { $n = $_GET[$type.'_data']['id']; $_GET[$type.$n.'_data'] = $_GET[$type.'_data']; } }
+$item_data = $_GET[$type.'_data'];
+if (is_string($atts)) { $field = $atts; $decimals = ''; $default = ''; $filter = ''; $id = 0; $part = 0; }
+else {
+$field = (isset($atts[0]) ? $atts[0] : '');
+foreach (array('decimals', 'default', 'filter') as $key) {
+$$key = (isset($atts[$key]) ? $atts[$key] : '');
+if (isset($atts[$key])) { unset($atts[$key]); } }
+$id = (int) (isset($atts[$attribute]) ? do_shortcode(str_replace(array('(', ')'), array('[', ']'), $atts[$attribute])) : 0);
+$part = (int) (isset($atts['part']) ? $atts['part'] : 0); }
+$field = str_replace('-', '_', format_nice_name($field));
+if ($field == '') { $field = $default_field; }
+if (($id > 0) && ((!isset($item_data['id'])) || ($id != $item_data['id']))) {
+foreach (array($type.'_id', $type.'_data') as $key) {
+if (isset($_GET[$key])) { $original[$key] = $_GET[$key]; } }
+if ((!isset($_GET[$type.$id.'_data'])) || (!isset($_GET[$type.$id.'_data']['id'])) || ($_GET[$type.$id.'_data']['id'] != $id)) {
+$_GET[$type.$id.'_data'] = (array) $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."contact_manager_".$table." WHERE id = $id", OBJECT); }
+$item_data = $_GET[$type.$id.'_data'];
+if ($attribute == 'id') { $_GET[$type.'_id'] = $id; $_GET[$type.'_data'] = $item_data; } }
+if ((!isset($item_data[$field])) && (isset($item_data['custom_fields'])) && (substr($field, 0, 13) == 'custom_field_')) {
+$item_custom_fields = (array) unserialize(stripslashes($item_data['custom_fields']));
+foreach ($item_custom_fields as $key => $value) { $item_data['custom_field_'.$key] = $value; } }
+$data = (isset($item_data[$field]) ? $item_data[$field] : '');
+if ($part > 0) { $data = explode(',', $data); $data = (isset($data[$part - 1]) ? trim($data[$part - 1]) : ''); }
+switch ($type) {
+case 'contact_form': case 'contact_form_category':
+$data = (string) $data;
+if ($data != '') { $data = contact_format_data($field, $data); }
+$data = (string) $data;
+if (($data == '') && (isset($item_data['category_id'])) && ($item_data['category_id'] > 0)) {
+if (is_string($atts)) { $atts = array($field); }
+$atts['category'] = $item_data['category_id'];
+$data = contact_form_category_data($atts); }
+elseif ($data == '') {
+if ((is_array($atts)) && (isset($atts['category']))) { unset($atts['category']); }
+$data = contact_data($atts); } break; }
+$data = (string) do_shortcode($data);
+if ($data == '') { $data = $default; }
+$data = contact_format_data($field, $data);
+$data = contact_filter_data($filter, $data);
+$data = contact_decimals_data($decimals, $data);
+foreach (array($type.'_id', $type.'_data') as $key) {
+if (isset($original[$key])) { $_GET[$key] = $original[$key]; } }
