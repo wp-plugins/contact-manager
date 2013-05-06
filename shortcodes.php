d318355dc08020ef152982ae7ee1722602e725de
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-06 11:01:06 +0000

    * Version 5.6 	* Improve the functions of the "forms.php" file 	* Minify the "date-picker.js" file 	* Improve the system of global variables
    git-svn-id: https://plugins.svn.wordpress.org/contact-manager/tags/5.6@708598 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ shortcodes.php
@@ -0,0 +1,35 @@
+<?php function contact_content($atts, $content) {
+global $wpdb;
+extract(shortcode_atts(array('id' => ''), $atts));
+$content = explode('[other]', do_shortcode($content));
+$forms = array_unique(preg_split('#[^0-9]#', $id, 0, PREG_SPLIT_NO_EMPTY));
+if (is_admin()) { if ((isset($GLOBALS['contact_form_id'])) && (in_array($GLOBALS['contact_form_id'], $forms))) { $n = 0; } else { $n = 1; } }
+else {
+$search_criteria = '';
+if (count($forms) > 0) {
+foreach ($forms as $form) { $search_criteria .= " OR form_id = ".$form; }
+$search_criteria = 'AND ('.substr($search_criteria, 4).')'; }
+$result = $wpdb->get_row("SELECT id FROM ".$wpdb->prefix."contact_manager_messages WHERE ip_address = '".$_SERVER['REMOTE_ADDR']."' $search_criteria", OBJECT);
+if ($result) { $n = 0; } else { $n = 1; } }
+if (!isset($content[$n])) { $content[$n] = ''; }
+return $content[$n]; }
+
+
+function contact_counter_tag($atts) {
+extract(shortcode_atts(array('data' => '', 'decimals' => '', 'filter' => ''), $atts));
+$string = $GLOBALS['contact_'.str_replace('-', '_', format_nice_name($data))];
+$string = contact_filter_data($filter, $string);
+$string = contact_decimals_data($decimals, $string);
+return $string; }
+
+
+function contact_counter($atts, $content) {
+$type = '';
+include dirname(__FILE__).'/counter.php';
+return $content[$k]; }
+
+
+function contact_form_counter($atts, $content) {
+$type = 'contact_form';
+include dirname(__FILE__).'/counter.php';
+return $content[$k]; }
---
+++ user-data.php
@@ -0,0 +1,42 @@
+<?php global $user_ID, $wpdb;
+$GLOBALS['user_data'] = (array) (isset($GLOBALS['user_data']) ? $GLOBALS['user_data'] : array());
+if ((!isset($GLOBALS['user_id'])) && (function_exists('is_user_logged_in')) && (is_user_logged_in())) { $GLOBALS['user_id'] = $user_ID; }
+if ((isset($GLOBALS['user_id'])) && ((!isset($GLOBALS['user_data']['ID'])) || ($GLOBALS['user_data']['ID'] != $GLOBALS['user_id']))) {
+$n = $GLOBALS['user_id']; $GLOBALS['user'.$n.'_data'] = (array) (isset($GLOBALS['user'.$n.'_data']) ? $GLOBALS['user'.$n.'_data'] : array());
+if ((isset($GLOBALS['user'.$n.'_data']['ID'])) && ($GLOBALS['user'.$n.'_data']['ID'] == $GLOBALS['user_id'])) { $GLOBALS['user_data'] = $GLOBALS['user'.$n.'_data']; }
+else { $GLOBALS['user_data'] = (array) $wpdb->get_row("SELECT * FROM ".$wpdb->base_prefix."users WHERE ID = ".$GLOBALS['user_id'], OBJECT); } }
+$user_data = $GLOBALS['user_data'];
+if (is_string($atts)) { $field = $atts; $default = ''; $filter = ''; $id = 0; }
+else {
+$field = (isset($atts[0]) ? $atts[0] : '');
+$default = (isset($atts['default']) ? $atts['default'] : '');
+$filter = (isset($atts['filter']) ? $atts['filter'] : '');
+$id = (int) (isset($atts['id']) ? do_shortcode(str_replace(array('(', ')'), array('[', ']'), $atts['id'])) : 0); }
+$field = str_replace('-', '_', format_nice_name($field));
+if ($field == '') { $field = 'login'; }
+switch ($field) {
+case 'date': case 'date_utc': $field = 'user_registered'; break;
+case 'email_address': $field = 'user_email'; break;
+case 'id': $field = 'ID'; break;
+case 'login': $field = 'user_login'; break;
+case 'website_url': $field = 'user_url'; break; }
+if (($id > 0) && ((!isset($user_data['ID'])) || ($id != $user_data['ID']))) {
+foreach (array('user_id', 'user_data') as $key) {
+if (isset($GLOBALS[$key])) { $original[$key] = $GLOBALS[$key]; } }
+if ((!isset($GLOBALS['user'.$id.'_data'])) || (!isset($GLOBALS['user'.$id.'_data']['ID'])) || ($GLOBALS['user'.$id.'_data']['ID'] != $id)) {
+$GLOBALS['user'.$id.'_data'] = (array) $wpdb->get_row("SELECT * FROM ".$wpdb->base_prefix."users WHERE ID = ".$id, OBJECT); }
+$user_data = $GLOBALS['user'.$id.'_data'];
+$GLOBALS['user_id'] = $id; $GLOBALS['user_data'] = $user_data; }
+$data = (isset($user_data[$field]) ? $user_data[$field] : '');
+switch ($field) {
+case 'first_name': case 'last_name': if (($data == '') && (isset($GLOBALS['user_id']))) {
+$n = $GLOBALS['user_id']; $GLOBALS['user'.$n.'_data'] = (array) (isset($GLOBALS['user'.$n.'_data']) ? $GLOBALS['user'.$n.'_data'] : array());
+if (isset($GLOBALS['user'.$n.'_data'][$field])) { $data = $GLOBALS['user'.$n.'_data'][$field]; }
+else {
+$result = $wpdb->get_row("SELECT meta_value FROM ".$wpdb->base_prefix."usermeta WHERE meta_key = '".$field."' AND user_id = ".$n, OBJECT);
+if ($result) { $data = $result->meta_value; $GLOBALS['user'.$n.'_data'][$field] = $data; } } } }
+$data = (string) do_shortcode($data);
+if ($data == '') { $data = $default; }
+$data = contact_filter_data($filter, $data);
+foreach (array('user_id', 'user_data') as $key) {
+if (isset($original[$key])) { $GLOBALS[$key] = $original[$key]; } }
