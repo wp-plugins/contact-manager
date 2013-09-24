2b0f63d49e9f4184915fdff4fffea117e2596ca7
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-24 21:46:43 +0000

    * Version 5.7 	* Add a meta box in the edit pages of posts and pages 	* Improve the support of HTML5 forms attributes
    git-svn-id: https://plugins.svn.wordpress.org/contact-manager/tags/5.7@778013 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/meta-box/fr.php
@@ -0,0 +1,5 @@
+<?php $links = array(
+'' => 'Documentation',
+'#forms' => 'Afficher un formulaire',
+'#sender-contents' => 'Afficher un contenu réservé aux expéditeurs',
+'#screen-options-wrap' => 'Cacher cette boîte');
