34bb9bbd32f942ef843b448e81b6da0693703464
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-22 20:31:24 +0000

    Improve SQL queries
    git-svn-id: https://plugins.svn.wordpress.org/contact-manager/tags/5.7.1@792008 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/sql-array.php
@@ -0,0 +1,8 @@
+<?php foreach ($table as $key => $value) {
+if (!isset($array[$key])) { $array[$key] = ''; }
+$sql[$key] = ($key == 'password' ? hash('sha256', $array[$key]) : $array[$key]);
+if (isset($value['type'])) {
+if ($value['type'] == 'int') { $sql[$key] = (int) $sql[$key]; }
+elseif ((strstr($value['type'], 'dec')) && (!is_numeric($sql[$key]))) { $sql[$key] = round(100*$sql[$key])/100; }
+elseif (($value['type'] == 'text') || ($value['type'] == 'datetime')) {
+$sql[$key] = "'".str_replace("'", "''", $sql[$key])."'"; } } }
