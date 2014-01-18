16c4100e9e5d665cc6e76e726bb3fc581db20fda
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-18 05:19:10 +0000

    * Version 5.8.6 	* Improve the cron
    git-svn-id: https://plugins.svn.wordpress.org/contact-manager/tags/5.8.6@840704 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/data.php
@@ -0,0 +1,19 @@
+<?php global $contact_manager_options;
+if (empty($contact_manager_options)) { $contact_manager_options = (array) get_option('contact_manager'); }
+if (is_string($atts)) { $field = $atts; $decimals = ''; $default = ''; $filter = ''; $part = 0; }
+else {
+$atts = array_map('contact_do_shortcode', (array) $atts);
+$field = (isset($atts[0]) ? $atts[0] : '');
+foreach (array('decimals', 'default', 'filter') as $key) { $$key = (isset($atts[$key]) ? $atts[$key] : ''); }
+$part = (int) (isset($atts['part']) ? $atts['part'] : 0); }
+$field = str_replace('-', '_', format_nice_name($field));
+if (($field == 'code') || (substr($field, -10) == 'email_body') || (substr($field, -19) == 'custom_instructions')) {
+$data = get_option(substr('contact_manager_'.$field, 0, 64)); }
+else { $data = (isset($contact_manager_options[$field]) ? $contact_manager_options[$field] : ''); }
+if ($part > 0) { $data = explode(',', $data); $data = (isset($data[$part - 1]) ? trim($data[$part - 1]) : ''); }
+$data = (string) do_shortcode($data);
+if ($data === '') { $data = $default; }
+$data = contact_format_data($field, $data);
+if ($data === '') { $data = $default; }
+$data = contact_filter_data($filter, $data);
+$data = contact_decimals_data($decimals, $data);
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php if ((isset($_GET['action'])) || (isset($_GET['url']))) {
+$file = 'wp-load.php'; $i = 0;
+while ((!file_exists($file)) && ($i < 8)) { $file = '../'.$file; $i = $i + 1; }
+include_once $file;
+if (isset($_GET['action'])) {
+switch ($_GET['action']) {
+case 'install': if ((isset($_GET['key'])) && ($_GET['key'] == md5(AUTH_KEY))) { install_contact_manager(); } break;
+default: if (!headers_sent()) { header('Location: '.HOME_URL); exit(); } } }
+elseif (isset($_GET['url'])) {
+$url = contact_decrypt_url($_SERVER['REQUEST_URI']);
+if (!headers_sent()) { header('Location: '.$url); exit(); } } }
+elseif (!headers_sent()) { header('Location: /'); exit(); }
